f8d9cdcddccd9673f25f0c588087a72b3dc1c638
fyx <info@yfyx.me>
2025-07-15 16:13:38 +0700

    Removes Dashboard component export
    Removes an export from the module's public interface, streamlining the API by removing an entry no longer managed through this aggregation point.

--- components/dashboard/index.ts
+++ components/dashboard/index.ts
@@ -1,5 +1,4 @@
 export { default as BottomNavigation } from '../ui/bottom-navigation';
-export { default as Dashboard } from './Dashboard';
 export { default as IngredientItem } from './IngredientItem';
 export { default as RecipeCard } from './RecipeCard';
 
